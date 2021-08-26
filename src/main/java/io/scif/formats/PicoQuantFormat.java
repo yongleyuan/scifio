72fadafb60fafaa8e7d97e2ee70ea1a3b9dd4b9b
Jack Yuan <yuan88@wisc.edu>
2021-08-25 23:52:13 -0500

    Create PicoQuantFormat.java class with generate info

---
+++ src/main/java/io/scif/formats/PicoQuantFormat.java
@@ -0,0 +1,19 @@
+package io.scif.formats;
+
+import io.scif.AbstractFormat;
+
+/**
+ * Handler plugin for the PicoQuant file format.
+ * 
+ * @author Jack Yuan
+ *
+ */
+public class PicoQuantFormat extends AbstractFormat {
+
+	@Override
+	protected String[] makeSuffixArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
