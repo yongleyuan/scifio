dd6f573ec2c8644174534f4fe7f2a0270fc667a4
Jack Yuan <yuan88@wisc.edu>
2021-08-26 08:48:15 -0500

    Add general layout

--- src/main/java/io/scif/formats/PicoQuantFormat.java
+++ src/main/java/io/scif/formats/PicoQuantFormat.java
@@ -1,10 +1,28 @@
 
 package io.scif.formats;
 
+import io.scif.AbstractChecker;
 import io.scif.AbstractFormat;
 import io.scif.AbstractMetadata;
+import io.scif.AbstractParser;
+import io.scif.AbstractTranslator;
+import io.scif.AbstractWriter;
+import io.scif.ByteArrayPlane;
+import io.scif.ByteArrayReader;
 import io.scif.Format;
+import io.scif.FormatException;
+import io.scif.ImageMetadata;
+import io.scif.Plane;
+import io.scif.config.SCIFIOConfig;
+import io.scif.formats.ICSFormat.Metadata;
 
+import java.io.IOException;
+import java.util.List;
+
+import net.imglib2.Interval;
+
+import org.scijava.io.handle.DataHandle;
+import org.scijava.io.location.Location;
 import org.scijava.plugin.Plugin;
 
 /**
@@ -33,6 +51,100 @@ public class PicoQuantFormat extends AbstractFormat {
 		public void populateImageMetadata() {
 			// TODO Auto-generated method stub
 
+		}
+
+	}
+
+	/**
+	 * SCIFIO file format Parser for PicoQuant images.
+	 */
+	public static class Parser extends AbstractParser<Metadata> {
+
+		@Override
+		protected void typedParse(DataHandle<Location> handle, Metadata meta,
+			SCIFIOConfig config) throws IOException, FormatException
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	/**
+	 * SCIFIO file format Checker for PicoQuant images.
+	 */
+	public static class Checker extends AbstractChecker {
+
+	}
+
+	/**
+	 * SCIFIO file format Reader for PicoQuant images
+	 */
+	public static class Reader extends ByteArrayReader<Metadata> {
+
+		@Override
+		public ByteArrayPlane openPlane(int imageIndex, long planeIndex,
+			ByteArrayPlane plane, Interval bounds, SCIFIOConfig config)
+			throws FormatException, IOException
+		{
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected String[] createDomainArray() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	/**
+	 * SCIFIO file format Writer for PicoQuant images.
+	 */
+	public static class Writer extends AbstractWriter<Metadata> {
+
+		@Override
+		protected String[] makeCompressionTypes() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected void writePlane(int imageIndex, long planeIndex, Plane plane,
+			Interval bounds) throws FormatException, IOException
+		{
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	/**
+	 * SCIFIO file format Translator for PicoQuant images.
+	 */
+	public static class Translator extends
+		AbstractTranslator<io.scif.Metadata, Metadata>
+	{
+
+		@Override
+		public Class<? extends io.scif.Metadata> source() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Class<? extends io.scif.Metadata> dest() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		protected void translateImageMetadata(List<ImageMetadata> source,
+			Metadata dest)
+		{
+			// TODO Auto-generated method stub
+			
 		}
 
 	}
