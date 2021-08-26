be74027692183f7e19908e29ab0f38e04f3667c3
Jack Yuan <yuan88@wisc.edu>
2021-08-26 06:15:06 -0500

    Add suffix array

--- src/main/java/io/scif/formats/PicoQuantFormat.java
+++ src/main/java/io/scif/formats/PicoQuantFormat.java
@@ -1,19 +1,40 @@
+
 package io.scif.formats;
 
 import io.scif.AbstractFormat;
+import io.scif.AbstractMetadata;
+import io.scif.Format;
+
+import org.scijava.plugin.Plugin;
 
 /**
  * Handler plugin for the PicoQuant file format.
  * 
  * @author Jack Yuan
- *
  */
+@Plugin(type = Format.class, name = "PicoQuant")
 public class PicoQuantFormat extends AbstractFormat {
+
+	// -- AbstractFormat Methods --
 
 	@Override
 	protected String[] makeSuffixArray() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[] { "ptu", "phu", "ht3", "pt3", "t3r" };
+	}
+
+	// -- Nested Classes --
+
+	/**
+	 * SCIFIO Metadata object for PicoQuant images.
+	 */
+	public static class Metadata extends AbstractMetadata {
+
+		@Override
+		public void populateImageMetadata() {
+			// TODO Auto-generated method stub
+
+		}
+
 	}
 
 }
