23e2596515de481605cafe3fac8febe3b877d851
Jack Yuan <yuan88@wisc.edu>
2021-09-16 02:55:28 -0500

    Create EXRFormat class with override AbstractFormat method

---
+++ src/main/java/io/scif/formats/EXRFormat.java
@@ -0,0 +1,15 @@
+package io.scif.formats;
+
+import io.scif.AbstractFormat;
+
+public class EXRFormat extends AbstractFormat {
+
+	// -- AbstractFormat Methods
+	
+	@Override
+	protected String[] makeSuffixArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
