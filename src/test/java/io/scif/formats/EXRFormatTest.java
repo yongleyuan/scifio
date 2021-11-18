a6f5f20db3cfff06a88a65bdb8ee96e0156e80f2
Jack Yuan <yuan88@wisc.edu>
2021-11-17 23:05:53 -0600

    Fix unit test

--- src/test/java/io/scif/formats/EXRFormatTest.java
+++ src/test/java/io/scif/formats/EXRFormatTest.java
@@ -3,8 +3,16 @@ package io.scif.formats;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import io.scif.Format;
 import io.scif.FormatException;
+import io.scif.ImageMetadata;
+import io.scif.Metadata;
+import io.scif.Parser;
 import io.scif.SCIFIO;
+
+import java.io.IOException;
 
 import org.scijava.io.location.FileLocation;
 import org.scijava.io.location.Location;
@@ -23,10 +31,17 @@ public class EXRFormatTest extends AbstractFormatTest {
 	}
 	
 	@Test
-	public void testOpenSampleImg() throws FormatException {
+	public void testOpenSampleImg() throws FormatException, IOException {
 		final String source = "/Users/jack/Projects/LOCI/scifio/src/test/resources/exr-example/MtTamWest.exr";
-		Location fileLoc = new FileLocation(source);
-		scifio.format().getFormat(fileLoc);
+		final Location exrFileLoc = new FileLocation(source);
+		final Format exrFormat = scifio.format().getFormat(exrFileLoc);
+		final Parser exrParser = exrFormat.createParser();
+		final Metadata meta = exrParser.parse(exrFileLoc);
+
+		// TODO write tests here specific to exr metadata
+		final ImageMetadata imgMeta = meta.get(0);
+		// print axis length to check
+		assertEquals(1214, imgMeta.getAxesLengths()[0]);
 	}
 
 }
